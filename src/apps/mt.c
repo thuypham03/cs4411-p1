5dadf8aa6c5eda2e816281b862632cefd824951b
thuypham03 <sophia.phamthuy@gmail.com>
2024-02-17 23:56:58 -0500

    initialize thread and thread_init

--- src/apps/mt.c
+++ src/apps/mt.c
@@ -8,7 +8,57 @@
 #include <egos/queue.h>
 #include <egos/context.h>
 
+/**** THREADS AND SEMAPHORES ****/
+typedef enum state {
+	RUNNING,
+	RUNNABLE,
+	TERMINATED
+} thread_state;
+
+typedef struct thread {
+	char *sp;
+    address_t base;
+	thread_state state;
+} thread_t;
+
+struct package {
+	thread_t *current;
+	struct queue *runnable_queue;
+} *global_package;
+
+
+void thread_init() {
+	// Initialize threading package
+	global_package = malloc(sizeof(package_t));
+	global_package->current = malloc(sizeof(thread_t));
+	global_package->runnable_queue = malloc(sizeof(struct queue));
+
+	global_package->current->base = NULL;
+	queue_init(global_package->runnable_queue);
+}
+
+void thread_create(void (*f)(void *arg), void *arg,
+					unsigned int stack_size);
+void thread_yield();
+void thread_exit();
+
+struct sema {
+    // your code here
+};
+
+void sema_init(struct sema *sema, unsigned int count);
+void sema_dec(struct sema *sema);
+void sema_inc(struct sema *sema);
+bool sema_release(struct sema *sema);
+
+/**** TEST SUITE ****/
+
+// your test code here, such as producer/consumer, read/write locks
+// dining philosophers and barber shop
 
 int main(int argc, char **argv){
-	return 0;
+    thread_init();
+    // your code here
+    thread_exit();
+    return 0;
 }
