c91728ae98dd57e9a179e33aaec84dfd0af17d2e
Thuy Pham <sophia.phamthuy@gmail.com>
2024-02-19 17:36:27 -0500

    Implement threads interface
    Co-authored-by: andrew <asc269@cornell.edu>

--- src/apps/mt.c
+++ src/apps/mt.c
@@ -7,8 +7,147 @@
 #include <string.h>
 #include <egos/queue.h>
 #include <egos/context.h>
+#include <earth/earth.h>
 
+void ctx_entry();
+void thread_init();
+void thread_create(void (*f)(void *arg), void *arg, unsigned int stack_size);
+void thread_yield();
+void thread_exit();
 
-int main(int argc, char **argv){
+/**** THREADS AND SEMAPHORES ****/
+typedef enum state
+{
+	RUNNABLE,
+	RUNNING,
+	TERMINATED
+} thread_state;
+
+typedef struct thread
+{
+	address_t sp;
+	char *base;
+	thread_state state;
+
+	void (*f)(void *arg);
+	void *arg;
+} thread_t;
+
+typedef struct scheduler
+{
+	thread_t *current;
+	thread_t *next;
+	struct queue *runnable_queue;
+	struct queue *terminated_queue;
+} scheduler;
+
+static scheduler *master;
+static int debug = 0;
+
+void ctx_entry()
+{
+	if (debug) sys_print("ctx_entry()\n");
+
+	master->current = master->next;
+	master->current->state = RUNNING;
+	master->current->f(master->current->arg);
+	thread_exit();
+}
+
+void thread_init()
+{
+	if (debug) sys_print("thread_init()\n");
+
+	// Initialize threading package
+	master = malloc(sizeof(scheduler));
+	master->runnable_queue = malloc(sizeof(struct queue));
+	queue_init(master->runnable_queue);
+	master->terminated_queue = malloc(sizeof(struct queue));
+	queue_init(master->terminated_queue);
+
+	master->current = malloc(sizeof(thread_t));
+	master->current->base = NULL;
+	master->next = NULL;
+}
+
+void thread_create(void (*f)(void *arg), void *arg, unsigned int stack_size)
+{
+	if (debug) sys_print("thread_create()\n");
+
+	master->current->state = RUNNABLE;
+	queue_add(master->runnable_queue, master->current);
+
+	master->next = malloc(sizeof(thread_t));
+	master->next->f = f;
+	master->next->arg = arg;	
+	master->next->base = malloc(stack_size);											 
+	master->next->sp = (address_t) &master->next->base[stack_size];
+	master->next->state = RUNNING;
+
+	// Switch from current to newly created thread (stack top = next->sp)
+	if (debug) sys_print("ctx_start()\n");
+	ctx_start(&master->current->sp, master->next->sp);
+	master->current = master->next;
+}
+
+void thread_yield()
+{
+	if (debug) sys_print("thread_yield()\n");
+
+	assert(master->current->state == RUNNING);
+	if (queue_empty(master->runnable_queue)) return;
+
+	master->current->state = RUNNABLE;
+	queue_add(master->runnable_queue, master->current);
+	master->next = (thread_t *) queue_get(master->runnable_queue);
+	master->next->state = RUNNING;
+
+	if (debug) sys_print("ctx_switch()\n");
+	ctx_switch(&master->current->sp, master->next->sp);
+	master->current = master->next;
+}
+
+void thread_exit()
+{
+	if (debug) sys_print("thread_exit()\n");
+	if (queue_empty(master->runnable_queue)) exit(0);
+
+	master->current->state = TERMINATED;
+	queue_add(master->terminated_queue, master->current);
+
+	master->next = (thread_t *) queue_get(master->runnable_queue);
+	master->next->state = RUNNING;
+
+	if (debug) sys_print("ctx_switch()\n");
+	ctx_switch(&master->current->sp, master->next->sp);
+	master->current = master->next;
+
+	// Next thread clean up last thread?
+	while (!queue_empty(master->terminated_queue)) {
+		thread_t *terminated_thread = (thread_t *) queue_get(master->terminated_queue);
+		free(terminated_thread->base);		
+		free((void *) terminated_thread);
+	}
+}
+
+/**** TEST SUITE ****/
+static void test_code(void *arg)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		printf("%s here: %d\n", arg, i);
+		thread_yield();
+	}
+	printf("%s done\n", arg);
+}
+
+int main(int argc, char **argv)
+{
+	thread_init();
+	thread_create(test_code, "thread 1", 16 * 1024);
+	thread_create(test_code, "thread 2", 16 * 1024);
+	test_code("main thread");
+	thread_exit();
 	return 0;
 }
