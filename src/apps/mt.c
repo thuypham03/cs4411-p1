7f4ff96a5f1402662f99a5b0f63aa54fab296a69
thuypham03 <sophia.phamthuy@gmail.com>
2024-02-20 13:10:31 -0500

    test

--- src/apps/mt.c
+++ src/apps/mt.c
@@ -8,126 +8,286 @@
 #include <egos/queue.h>
 #include <egos/context.h>
 #include <earth/earth.h>
+/* Threads and semaphores in user space.
+ */
 
-void ctx_entry();
-void thread_init();
-void thread_create(void (*f)(void *arg), void *arg, unsigned int stack_size);
-void thread_yield();
-void thread_exit();
 
-/**** THREADS AND SEMAPHORES ****/
-typedef enum state
-{
-	RUNNABLE,
+#define STACKSIZE 16 * 1024
+#define NSLOTS 3
+
+/**
+ * Thread package implementation
+ */
+
+enum state{
+    READY,
 	RUNNING,
-	TERMINATED
-} thread_state;
+    STOPPED,
+	TERMINATED,
+	BLOCKED,
+};
 
-typedef struct thread
-{
-	address_t sp;
-	char *base;
-	thread_state state;
 
-	void (*f)(void *arg);
+/**
+ *  start address of stack : base pointer
+ *  top address of stack : top pointer
+ *  state of the thread : state
+ *  thread id: id
+ *  argument : arg pointer
+ *  instruction pointer: ip
+ *
+ */
+struct thread{
+    address_t base;
+    char *stack;
+	enum state state;
 	void *arg;
-} thread_t;
+	void (*ip)(void *);
+    int id; // thread id for debugging purpose
 
-typedef struct scheduler
-{
-	thread_t *current;
-	thread_t *next;
-	struct queue *runnable_queue;
-	struct queue *terminated_queue;
-} scheduler;
+};
 
-static scheduler *master;
-static int debug = 0;
+/**
+ * Monitor: Global struct to keep track of current running thread,
+ * ready queue, and exited thread queue
+ *
+ */
+struct monitor {
+    struct thread *current_t;
+    struct thread * next_t;
+    struct queue* thread_ready;
+    struct queue* thread_exited;
+};
 
-void ctx_entry()
-{
-	if (debug) sys_print("ctx_entry()\n");
 
-	master->current = master->next;
-	master->current->state = RUNNING;
-	master->current->f(master->current->arg);
-	thread_exit();
+static struct monitor* monitor;
+void thread_exit();
+void thread_yield();
+static void thread_schedule();
+void ctx_entry(void);
+static struct thread* findRunnable();
+static int id;
+
+/**
+ *  thread_init initializes the threading package
+ */
+void thread_init(){
+    // Initialize all global data structures for threading
+    monitor = malloc(sizeof(struct monitor));
+    monitor->thread_ready = malloc(sizeof(struct queue));
+    monitor->thread_exited = malloc(sizeof(struct queue));
+    monitor->current_t = malloc(sizeof(struct thread));
+    id = 0;
+    monitor->current_t->id = id;
+
+    queue_init(monitor->thread_ready);
+    queue_init(monitor->thread_exited);
+    if(!monitor->thread_ready || !monitor->thread_exited) {
+        perror("Allocations for thread queues failed!");
+        sys_exit(1);
+    }
+
+
 }
 
-void thread_init()
-{
-	if (debug) sys_print("thread_init()\n");
-
-	// Initialize threading package
-	master = malloc(sizeof(scheduler));
-	master->runnable_queue = malloc(sizeof(struct queue));
-	queue_init(master->runnable_queue);
-	master->terminated_queue = malloc(sizeof(struct queue));
-	queue_init(master->terminated_queue);
-
-	master->current = malloc(sizeof(thread_t));
-	master->current->base = NULL;
-	master->next = NULL;
+/**
+ * thread_create creates a thread
+ */
+void thread_create(void (*f)(void *arg), void *arg,
+				   unsigned int stack_size){
+    struct thread * t = malloc(sizeof(struct thread));
+    id++;
+    t->id = id;
+    t->ip = f;
+    t->stack = malloc(STACKSIZE);
+    t->base = (address_t) &t->stack[STACKSIZE];
+    t->arg = arg;
+    t->state = READY;
+    queue_add(monitor->thread_ready, t);
+    thread_schedule();
 }
 
-void thread_create(void (*f)(void *arg), void *arg, unsigned int stack_size)
-{
-	if (debug) sys_print("thread_create()\n");
+/**
+ * ctx_entry called by ctx_start to
+ * Invoke a new thread and call f(), then exit()
+ */
+void ctx_entry(void) {
+    /**
+     * Set current running thread as stopped
+     */
+    monitor->current_t->state = STOPPED;
+    /**
+     * Put stopped thread to ready queue
+     */
+    queue_add(monitor->thread_ready, monitor->current_t);
+    monitor->current_t = monitor->next_t;
+    monitor->current_t->state = RUNNING;
+    /**
+     * Invoke f()
+     */
+    (monitor->current_t->ip)(monitor->current_t->arg);
+    /**
+     * Exit current thread
+     */
+    thread_exit();
 
-	master->current->state = RUNNABLE;
-	queue_add(master->runnable_queue, master->current);
-
-	master->next = malloc(sizeof(thread_t));
-	master->next->f = f;
-	master->next->arg = arg;	
-	master->next->base = malloc(stack_size);											 
-	master->next->sp = (address_t) &master->next->base[stack_size];
-	master->next->state = RUNNING;
-
-	// Switch from current to newly created thread (stack top = next->sp)
-	if (debug) sys_print("ctx_start()\n");
-	ctx_start(&master->current->sp, master->next->sp);
-	master->current = master->next;
 }
 
-void thread_yield()
-{
-	if (debug) sys_print("thread_yield()\n");
+/**
+ * Helper function for scheduling threads
+ */
+static void thread_schedule(){
+    struct thread *old = monitor->current_t;
 
-	assert(master->current->state == RUNNING);
-	if (queue_empty(master->runnable_queue)) return;
+    monitor->next_t = findRunnable();
+    /**
+     * If current thread is also next runnable thread, return
+     */
+    if(old == monitor->next_t) return;
+    /**
+     * Schedule next runnable thread
+     */
+    if(monitor->next_t->state == READY) {
+        /**
+         * This thread never got to run before
+         */
+        ctx_start(&(old->base), (monitor->next_t->base));
+    } else if(monitor->next_t->state == STOPPED) {
+        /**
+         * This thread has already run before
+         */
+        if(old->state != BLOCKED && old->state != TERMINATED) {
+            old->state = STOPPED;
+            queue_add(monitor->thread_ready, old);
+        }
+        monitor->next_t->state = RUNNING;
+        monitor->current_t = monitor->next_t;
+        ctx_switch(&(old->base), (monitor->next_t->base));
+    }
 
-	master->current->state = RUNNABLE;
-	queue_add(master->runnable_queue, master->current);
-	master->next = (thread_t *) queue_get(master->runnable_queue);
-	master->next->state = RUNNING;
-
-	if (debug) sys_print("ctx_switch()\n");
-	ctx_switch(&master->current->sp, master->next->sp);
-	master->current = master->next;
 }
+void thread_yield() {
+    /**
+     * If ready queue is empty and current thread is not runnable exit with 1
+     */
+    if(queue_empty(monitor->thread_ready)) {
+        if(monitor->current_t->state == TERMINATED || monitor->current_t->state == BLOCKED) sys_exit(1);
+        else return;
+    }
+    /**
+     * Clear the exited queue and free their stacks if current thread is runnable
+     */
+    if(monitor->current_t->state != BLOCKED && monitor->current_t->state != TERMINATED) {
+        struct thread * cur;
+        while((cur = queue_get(monitor->thread_exited)) != NULL) {
+            if(cur->stack)
+                free(cur->stack);
+            free(cur);
+        }
+    }
+    /**
+     * Schedule for next runnable thread
+     */
+    thread_schedule();
 
-void thread_exit()
-{
-	if (debug) sys_print("thread_exit()\n");
-	if (queue_empty(master->runnable_queue)) exit(0);
+}
+void thread_exit() {
+    /**
+     * Terminated current running thread, added to exited queue, schedule for next runnable thread
+     */
+    monitor->current_t->state = TERMINATED;
+    queue_add(monitor->thread_exited, monitor->current_t);
+    thread_yield();
 
-	master->current->state = TERMINATED;
-	queue_add(master->terminated_queue, master->current);
+}
+/**
+ * Helper function to find next runnable thread from ready queue
+ * @return a pointer to a runnable thread
+ */
+struct thread*
+findRunnable(){
+    /**
+     * ready queue is empty means
+     * either return current running thread
+     * or whole program exits
+     */
+    if(queue_empty(monitor->thread_ready)) {
+        if (monitor->current_t->state != TERMINATED && monitor->current_t->state != BLOCKED) {
+            return monitor->current_t;
+        } else {
+            sys_exit(1);
+        }
+    }
+    /**
+     * get next runnable thread from ready queue
+     */
+    struct thread *t1;
+    t1 = queue_get(monitor->thread_ready);
 
-	master->next = (thread_t *) queue_get(master->runnable_queue);
-	master->next->state = RUNNING;
+    return t1;
+}
+/***
+ * The end of thread package
+ *
+ */
+/**
+ * Semaphores implementations
+ *
+ */
+struct sema {
+    int count;
+    struct queue* queue_wait;
+};
 
-	if (debug) sys_print("ctx_switch()\n");
-	ctx_switch(&master->current->sp, master->next->sp);
-	master->current = master->next;
+/**
+ * sema_init for semaphores packages initialization
+ * @param sema
+ * @param count
+ */
+void sema_init(struct sema *sema, unsigned int count) {
 
-	// Next thread clean up last thread?
-	while (!queue_empty(master->terminated_queue)) {
-		thread_t *terminated_thread = (thread_t *) queue_get(master->terminated_queue);
-		free(terminated_thread->base);		
-		free((void *) terminated_thread);
-	}
+    sema->queue_wait = malloc(sizeof(struct queue));
+    queue_init(sema->queue_wait);
+    if(sema->queue_wait == NULL) {
+        free(sema);
+        perror("allocation Failed\n");
+        return;
+    }
+    sema->count = count;
+}
+/**
+ * sema_dec takes a pointer to sema, if count is positive, decrement its count
+ * otherwise wait for the count to increment
+ * @param sema
+ */
+
+void sema_dec(struct sema* sema){
+    if(sema->count == 0){
+        monitor->current_t->state = BLOCKED;
+        queue_add(sema->queue_wait, monitor->current_t);
+        
+        thread_yield();
+    } else {
+        sema->count--;
+    }
+
+}
+/**
+ * sema_inc takes a pointer to sema, if wait queue is not empty, put a thread to the ready queue
+ * otherwise increment its count
+ * @param sema
+ */
+void sema_inc(struct sema* sema) {
+    struct thread *thread1;
+
+    if(!queue_empty(sema->queue_wait)) {
+        thread1 = (struct thread *)queue_get(sema->queue_wait);
+        thread1->state = STOPPED;
+        queue_add(monitor->thread_ready,thread1);
+    } else {
+        sema->count++;
+    }
+
 }
 
 /**** TEST SUITE ****/
@@ -142,12 +302,61 @@ static void test_code(void *arg)
 	printf("%s done\n", arg);
 }
 
+/**
+ * Test for consumer and producer
+ */
+static struct sema s_empty, s_full, s_lock;
+static unsigned int in, out;
+static char *slots[NSLOTS];
+static void consumer(void *arg){
+
+    unsigned int i;
+    for (i = 0; i < 5; i++) {
+// first make sure there’s something in the buffer
+        sema_dec(&s_full);
+
+// now grab an entry to the queue
+        sema_dec(&s_lock);
+
+        void *x = slots[out++];
+        printf("%s: got ’%s’\n", arg, x);
+        if (out == NSLOTS) out = 0;
+        sema_inc(&s_lock);
+// finally, signal producers
+        sema_inc(&s_empty);
+    }
+}
+
+
+static void producer(void *arg){
+    for (;;) {
+// first make sure there’s an empty slot.
+        sema_dec(&s_empty);
+// now add an entry to the queue
+        sema_dec(&s_lock);
+        slots[in++] = arg;
+        if (in == NSLOTS) in = 0;
+
+        sema_inc(&s_lock);
+// finally, signal consumers
+        sema_inc(&s_full);
+    }
+
+}
+void test_producer(){
+    // Initialize threading package
+    thread_init();
+
+    // Initialize semaphores for producer-consumer
+    sema_init(&s_lock, 1);
+    sema_init(&s_full, 0);
+    sema_init(&s_empty, NSLOTS);
+    thread_create(consumer, "consumer 1", 16 * 1024);
+    producer("producer 1");
+}
+
 int main(int argc, char **argv)
 {
-	thread_init();
-	thread_create(test_code, "thread 1", 16 * 1024);
-	thread_create(test_code, "thread 2", 16 * 1024);
-	test_code("main thread");
-	thread_exit();
+	test_producer();
 	return 0;
 }
